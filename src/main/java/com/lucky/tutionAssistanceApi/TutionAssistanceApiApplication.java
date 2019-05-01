1dbf321bc813acef79d9c559d176fb15f8a97c51
shahidaLucky <shahidakawsar@gmail.com>
2019-05-01 19:32:09 +0600

    Initial Commit.

---
+++ src/main/java/com/lucky/tutionAssistanceApi/TutionAssistanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.lucky.tutionAssistanceApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutionAssistanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutionAssistanceApiApplication.class, args);
+	}
+
+}
